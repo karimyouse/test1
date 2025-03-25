973e0305a00eb6e4347002f49dd06af50913cabd
karimyouse <103128562+karimyouse@users.noreply.github.com>
2025-03-25 14:19:58 +0200

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,49 +1,55 @@
 <?php
+ 
+ namespace App\Http\Controllers;
+ 
+ use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\DB;
+ use App\Models\Task;
+ 
+ 
+ class TaskController extends Controller
+ {
+      public function index(){
+        //      $tasks = DB::table('tasks')->get();
+             $tasks = Task::all();
 
-namespace App\Http\Controllers;
+             return view('tasks' , compact('tasks')); 
+     }
+ 
+     public function create(){
+             $task_name = $_POST['name'];
+        //      DB::table('tasks')->insert(['name' =>$task_name]);
+                $task = new Task();
+                $task->name = $task_name;
+                $task->save(); 
+ 
+             return redirect()->back();
+     }
+ 
+     public function destroy($id){
+        //      DB::table('tasks')->where('id',$id)->delete();
+                $tasks = Task::find($id);
+                $tasks->delete();
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-class TestController extends Controller
-{
-    public function index(){
+             return redirect()->back(); 
+ }
+     public function edit($id){
+        //      $task = DB::table('tasks')->where('id',$id)->first();
+        //      $tasks = DB::table('tasks')->get();
+                   $task = Task::find($id);
+                         $tasks = Task::all();
 
-        $tests = DB:: table(table: 'tests')->get();
-
-    return view('tests',data: compact(var_name: 'tests'));
-
-    }
-
-    public function create(){
-        $task_name = $_POST['name'];
-    DB::table('tests')->insert(['name' => $task_name]);
-    return redirect()->back();
-    }
-
-    public function destroy($id){
-
-        DB:: table('tests')->where('id',$id)->delete();
-
-    return redirect()->back();
-
-    }
-    
-
-    public function edit($id){
-        $test = DB :: table ('tests')->where('id',$id)->first();
-    $tests = DB :: table('tests')-> get();
-    return view('tests',compact('test','tests'));
-    }
-
-    public function update(){
-        $id = $_POST['id'];
-
-    DB:: table('tests')->where('id','=',$id)->update(['name'=>$_POST['name']]);
-
-    return redirect('tests');
-    }
-
-
-
-
-}
+ 
+             return view('tasks' , compact('task' , 'tasks'));
+ }
+     public function update($id){
+             $id = $_POST['id'];
+        //      DB::table('tasks')->where('id',$id)->update(['name' => $_POST['name']]);
+                $task = Task::find($id);
+                $task->name = $_POST['name'];
+                $task->save();
+ 
+             return redirect('tasks');
+ }
+ }
+ 
