d5c687675f821f5286e8f84ae92616ba3dae8ee5
karimyouse <103128562+karimyouse@users.noreply.github.com>
2025-03-25 14:37:01 +0200

    Update users.blade.php

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,103 +1,103 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel Quickstart - Basic</title>
-
-    <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-        body {
-            font-family: 'Lato';
-        }
-    </style>
-</head>
-     
 @extends('layouts.app')
 @section('content')
-<div class="container mt-4">
-    <h1>User List App</h1>
+    <div class="container mt-4">
+        <h1>User List App</h1>
         <div class="offset-md-2 col-md-8">
             <div class="card">
                 @if(isset($user))
-
                 <div class="card-header">
-                    Update Task
+                    Update User
                 </div>
                 <div class="card-body">
-                    <!-- Update Task Form -->
+                    <!-- Update User Form -->
                     <form action="{{url('update')}}" method="POST">
-                        @csrf 
-                        <input type="hidden" name="id" value="{{$user->id}}">
-                        <!-- Task Name -->
+                    @csrf
+
+                        <input type="hidden" name = "id" value="{{$user->id}}">                        <!-- User Name -->
+                        
                         <div class="mb-3">
-                            <label for="user-name" class="form-label">Task</label>
+                            <label for="user-name" class="form-label">Name</label>
                             <input type="text" name="name" id="user-name" class="form-control" value="{{$user->name}}">
                         </div>
 
-                        <!-- Update Task Button -->
+                        <!-- User Email -->
+                        <div class="mb-3">
+                            <label for="user-email" class="form-label">Email</label>
+                            <input type="email" name="email" id="user-email" class="form-control" value="{{$user->email}}">
+                        </div>
+
+                        <!-- User Password -->
+                        <div class="mb-3">
+                            <label for="user-password" class="form-label">Password</label>
+                            <input type="password" name="password" id="user-password" class="form-control" value="{{$user->password}}">
+                        </div>
+
+                        <!-- Update User Button -->
                         <div>
                             <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-plus me-2"></i>Update Task
+                                <i class="fa fa-plus me-2"></i>Update User
                             </button>
                         </div>
                     </form>
                 </div>
-
-
-
                 @else
                 <div class="card-header">
-                    New Task
+                    New User
                 </div>
                 <div class="card-body">
-                    <!-- New Task Form -->
+                    <!-- New User Form -->
                     <form action="create" method="POST">
-                        @csrf 
-                        <!-- Task Name -->
+                    @csrf
+                        <!-- User Name -->
                         <div class="mb-3">
-                            <label for="user-name" class="form-label">Task</label>
+                            <label for="user-name" class="form-label">Name</label>
                             <input type="text" name="name" id="user-name" class="form-control" value="">
                         </div>
 
-                        <!-- Add Task Button -->
+                        <!-- User Email -->
+                        <div class="mb-3">
+                            <label for="user-email" class="form-label">Email</label>
+                            <input type="email" name="email" id="user-email" class="form-control" value="">
+                        </div>
+
+                        <!-- User Password -->
+                        <div class="mb-3">
+                            <label for="user-password" class="form-label">Password</label>
+                            <input type="password" name="password" id="user-password" class="form-control" value="">
+                        </div>
+
+                        <!-- Add User Button -->
                         <div>
                             <button type="submit" class="btn btn-primary">
-                                <i class="fa fa-plus me-2"></i>Add Task
+                                <i class="fa fa-plus me-2"></i>Add User
                             </button>
                         </div>
                     </form>
                 </div>
                 @endif
-
             </div>
 
-            <!-- Current Tasks -->
+            <!-- Current Users -->
             <div class="card mt-4">
                 <div class="card-header">
-                    Current Tasks
+                    Current Users
                 </div>
                 <div class="card-body">
                     <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th>Task</th>
+                                <th>User</th>
+                                <th>Email</th>
                                 <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user) 
-                            
+                        @if(isset($users) && count($users) > 0)
+                            @foreach($users as $user)
+
                             <tr>
                                 <td>{{$user->name}}</td>
+                                <td>{{$user->email}}</td>
                                 <td>
                                     <form action="/delete/{{$user->id}}" method="POST" class="d-inline">
                                         @csrf
@@ -113,36 +113,13 @@
                                     </form>
                                 </td>
                             </tr>
-                                  @endforeach
 
-                             
-                            <tr>
-                                <td>Task 2</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Task 3</td>
-                                <td>
-                                    <form action="#" method="POST" class="d-inline">
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash me-2"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
+                            @endforeach
+                            @endif  
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
-
 @endsection
-    
-    
