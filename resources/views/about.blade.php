2a0ea5346fd511d2d99ff2edf9463c4c09f5e7d2
karimyouse <103128562+karimyouse@users.noreply.github.com>
2025-03-25 14:36:22 +0200

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,24 +2,22 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>web laravel</title>
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <h1>Hello , {{ $name }}</h1>
+    <h1>Hello, {{$name}}</h1>
+
     <form action="about" method="post">
-        @csrf
-        <input type="text" name="name" id="name"><br>
-     <select name="departs" id="">
-        @foreach ($departs as $key => $depart)
-         <option value="{{ $key }}"> {{ $depart }} </option>
-         
-
+        @csrf 
+     <input type="text" name="name" id="name"><br><br>
+     <select name="department" id="department">
+        @foreach ($departments as $key => $department)
+        <option value="{{$key}}">{{$department}}</option>
         @endforeach
+     </select><br><br>
+     <input type="submit" value="send">
 
-     </select> <br>
-     <input type="submit" value="Send"><br>
- </form>
+    </form>
 </body>
-</html>
+</html>  
