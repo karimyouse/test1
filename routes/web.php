516a037771694145bfef81c6869c88739ba52f2d
karimyouse <103128562+karimyouse@users.noreply.github.com>
2025-03-25 14:38:04 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,57 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/about' , function(){
+Route::get('/about', function () {
     $name = 'Karim';
-    $departs = [
-        '1' => 'Tichnical' ,
-        '2' => 'Finacial' ,
-        '3' => 'Seles' ,
-    ];
-    
-   return view('about' , data: compact('name' , 'departs'));
-
-});
-
-Route::post('/about' , function(){
+    $departments = [ 
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+     ]; 
+      
+   
+    return view('about', data: compact('name' , 'departments')); 
+}); 
+Route::post('/about', function () {
     $name = $_POST['name'];
-    $departs = [
-        '1' => 'Tichnical' ,
-        '2' => 'Finacial' ,
-        '3' => 'Seles' ,
+    $departments = [ 
+        '1' => 'Tichnical',
+        '2' => 'Financial',
+        '3' => 'Sales',
+     ]; 
 
-    ];
-    return view('about' , data:compact('name','departs'));
-
+    return view('about', compact('name'));
+}); 
+Route::get('app', function () {
+    return view('layouts.app');
 });
 
-Route::get('tests' , [TestController::class, 'index']);
 
-Route::post('create' , [TestController::class, 'create']);
+Route::get('tasks', action: [TaskController::class , 'index']);
 
-Route :: post ('delete/{id}',[TestController::class,'destroy']);
+Route::post('create',action: [TaskController::class , 'create']);  
 
-Route :: post ('edit/{id}', [TestController::class,'edit']);
+Route::post('delet/{id}', action: [TaskController::class, 'destroy']);  
+
+Route::post('edit/{id}', action: [TaskController::class, 'edit']);
+
+Route::post('update/{id}', action: [TaskController::class, 'update']);  
 
 
-Route :: post('update',[TestController::class,'update']);
 
-Route :: get('app',function(){
-    return view('layouts.app');
-} );
+ Route::get('users', [UserController:: class, 'index']);
 
-Route::get('users',[UserController::class,'index']);
-Route::post('create',[UserController::class,'create']);
-Route::post('delete/{id}',[UserController::class,'destroy']);
-Route::post('edit/{id}',[UserController::class,'edit']);
-Route::post('update',[UserController::class,'update']);
+Route::post('create',  [UserController:: class, 'create']);
+
+Route::post('delete/{id}', [UserController:: class, 'destroy']);
+
+Route::post('edit/{id}',[UserController:: class, 'edit']);
+
+Route::post('update', [UserController:: class, 'update'] );
